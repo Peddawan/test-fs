4e5253ac5b6f846e54ae7fb0a3587403fc68a4e0
Peddawan <peter.hoess@fs-students.de>
2025-02-15 11:51:24 +0000

    First terraform storage bucket exercise

---
+++ main.tf
@@ -0,0 +1,37 @@
+# Declarative approach: Let the earth be like this. 
+
+# We need a certain provider, which is google
+terraform {
+    required_providers {
+      
+      google = {
+        source = "hashicorp/google"
+        version = "~> 5.0"
+      }
+    }  
+}
+
+provider "google" {
+    project = "my-own-project-1-451009" # Own project ID
+    region = "us-west1"
+}
+
+# We give the bucket a name and create Google Cloud Storage bucket
+resource "google_storage_bucket" "mybucket" {
+    name = "my-terraform-bucket-999998999" # Ensure unique ID
+    location = "us-west1"
+
+  versioning {
+    enabled = true
+  }
+
+  labels = {
+    environment = "student-lab"
+    
+  }
+}
+
+# Define a output variable bucket_name
+output "bucket_name" {
+    value = google_storage_bucket.mybucket.name
+}
